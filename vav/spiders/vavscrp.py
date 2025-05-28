8df09dce5f1eba19b7988aee2a145f391548f508
Olexander <afurmanenko@gmail.com>
2025-05-28 20:34:13 +0300

    fix: update vacancy ID extraction for ru site

--- vav/spiders/vavscrp.py
+++ vav/spiders/vavscrp.py
@@ -34,7 +34,7 @@ class VavscrpSpider(scrapy.Spider):
         current_vacancies = set()
 
         for link in links:
-            vac_id = link.split('ua')[-1].replace('/', '').replace('-', '')
+            vac_id = link.split('ru')[-1].replace('/', '').replace('-', '')
             current_vacancies.add(vac_id)
 
             if vac_id not in self.existing_vacancies:
